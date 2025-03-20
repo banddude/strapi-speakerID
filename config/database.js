0b0c3ec0fd4449aac50974197555aecaa473eb49
Mike Shaffer <mikejshaffer@gmail.com>
2025-03-19 20:08:10 -0700

    Update database configuration for Supabase

--- config/database.js
+++ config/database.js
@@ -2,9 +2,17 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            connectionString: env('DATABASE_URL')
+            host: env('DATABASE_HOST', 'ixcehxfeiwkhawkxauvs.supabase.co'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'postgres'),
+            user: env('DATABASE_USERNAME', 'postgres'),
+            password: env('DATABASE_PASSWORD', ''),
+            schema: env('DATABASE_SCHEMA', 'public'),
+            ssl: {
+                rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+            },
         },
-        debug: true,
+        debug: false,
         pool: { min: 0, max: 7 },
     }
 });
